afef231e952c17eaddcf648d702bd414d5299294
jaizet <3978174@gmail.com>
2019-01-26 19:06:14 +0300

    Initial backend

--- src/main/java/com/javathon/team/JavaThon/repositories/HackathonRepository.java
+++ src/main/java/com/javathon/team/JavaThon/repositories/HackathonRepository.java
@@ -3,5 +3,5 @@ package com.javathon.team.JavaThon.repositories;
 import com.javathon.team.JavaThon.entities.Hackathon;
 import org.springframework.data.repository.CrudRepository;
 
-public interface HackathonsRepository extends CrudRepository<Hackathon, Long> {
+public interface HackathonRepository extends CrudRepository<Hackathon, Long> {
 }
--- src/main/java/com/javathon/team/JavaThon/repositories/ParticipantRepository.java
+++ src/main/java/com/javathon/team/JavaThon/repositories/ParticipantRepository.java
@@ -3,5 +3,5 @@ package com.javathon.team.JavaThon.repositories;
 import com.javathon.team.JavaThon.entities.Participant;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ParticipantsRepository extends CrudRepository<Participant, Long> {
+public interface ParticipantRepository extends CrudRepository<Participant, Long> {
 }
