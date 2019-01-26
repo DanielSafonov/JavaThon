b716c212c811bc22fa85acd4c6b9127ddcad2d0c
jaizet <3978174@gmail.com>
2019-01-26 18:47:51 +0300

    Initial backend

--- src/main/java/com/javathon/team/JavaThon/repositories/ParticipantsRepository.java
+++ src/main/java/com/javathon/team/JavaThon/repositories/ParticipantsRepository.java
@@ -3,5 +3,5 @@ package com.javathon.team.JavaThon.repositories;
 import com.javathon.team.JavaThon.entities.Participants;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ParticipantsRepository extends CrudRepository<Participants, L> {
+public interface ParticipantsRepository extends CrudRepository<Participants, Long> {
 }
