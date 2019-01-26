4a849dc28bd1feddbc7485924cf81d5714ae66ec
DanielSafonov <imdaneel1798@gmail.com>
2019-01-26 15:10:36 +0300

    Spring Initializr

---
+++ src/main/java/com/javathon/team/JavaThon/JavaThonApplication.java
@@ -0,0 +1,14 @@
+package com.javathon.team.JavaThon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaThonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaThonApplication.class, args);
+	}
+
+}
+
