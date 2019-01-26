bb480e8a8b7f7144b4d68b35f4fc0a70e80e2aa3
jaizet <3978174@gmail.com>
2019-01-26 19:31:22 +0300

    Initial backend

--- src/main/java/com/javathon/team/JavaThon/repositories/UserRepository.java
+++ src/main/java/com/javathon/team/JavaThon/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.javathon.team.JavaThon.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository  extends CrudRepository<User, Long> {
+    public User findByPhoneNumber(String phoneNumber);
 }
--- src/main/java/com/javathon/team/JavaThon/services/UsersService.java
+++ src/main/java/com/javathon/team/JavaThon/services/UsersService.java
@@ -1,19 +1,20 @@
 package com.javathon.team.JavaThon.services;
 
-import com.javathon.team.JavaThon.repos.UsersRepo;
+import com.javathon.team.JavaThon.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service //Класс-сервис
+@Service
 public class UsersService implements UserDetailsService {
-    @Autowired //Автосвязывание сервиса и репозитория доступа к профилям пользоввателей
-    private UsersRepo usersRepo;
 
-    @Override //Переопределение метода возврата профиля пользователя по username
+    @Autowired
+    private UserRepository usersRepo;
+
+    @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return usersRepo.findByUsername(username);
+        return usersRepo.findByPhoneNumber(username);
     }
 }
