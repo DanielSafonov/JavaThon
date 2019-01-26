e7a893de77067acc79be5a2dafb319d0bf01c8e2
DanielSafonov <imdaneel1798@gmail.com>
2019-01-26 19:01:28 +0300

    Прогрузил репозиторий и поменял все на множественное число

--- src/main/java/com/javathon/team/JavaThon/config/MvcConfig.java
+++ src/main/java/com/javathon/team/JavaThon/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.javathon.team.JavaThon.Config;
+package com.javathon.team.JavaThon.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
